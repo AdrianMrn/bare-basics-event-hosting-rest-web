24b82f204fad9cbd2a44c3263bbe89b84750b3ad
AdrianMrn <adriaanmarain300@gmail.com>
2018-08-05 18:12:35 +0200

    gitignore

--- resources/assets/js/api.js
+++ resources/assets/js/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function getTest() {
-    axios.get('https://www.google.com')
+    axios.get('http://localhost:8000/api/test')
         .then(function (response) {
             console.log(response);
         })
