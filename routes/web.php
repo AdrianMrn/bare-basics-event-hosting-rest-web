9bfa719a7da635532c1e0c6fe8b4d7243480c25a
AdrianMrn <adriaanmarain300@gmail.com>
2018-08-23 22:00:12 +0200

    cors fix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, HEAD, PATCH, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
 // This will route everything to the 'spa' view
 Route::get('/{path?}', function () {
     return view('spa');
