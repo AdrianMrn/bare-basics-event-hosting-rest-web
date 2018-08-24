1b51bbf6177a2ba741ad19009d424c0462017f97
AdrianMrn <adriaanmarain300@gmail.com>
2018-08-24 10:16:02 +0200

    :/

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -23,11 +23,8 @@ class EventController extends Controller
 
     public function show($id, Request $request){
         $event = Event::where('slug', $id)->firstOrFail();
-        $event->imageUrl = $event->getMedia()[0];
-        return ($event->imageUrl);
-        if (isset($event->imageUrl)) {
-            $event->imageUrl = $event->imageUrl->getUrl();
-        }
+        $images = $event->getMedia();
+        $event->imageUrl = $image[0]->getUrl();
 
         return JsonResponse::create(['error' => false, 'eventData' => $event]);
     }
