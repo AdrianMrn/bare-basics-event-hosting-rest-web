9c766a2a98e15c0b9bb7d3df7a011291dd87afbf
AdrianMrn <adriaanmarain300@gmail.com>
2018-08-29 18:37:52 +0200

    added playstore link to remote user create

--- resources/views/accountCreated.blade.php
+++ resources/views/accountCreated.blade.php
@@ -1,13 +1,16 @@
 <h2>Hi!</h2>
 
 <!-- TODO: BBEvents logo -->
+<div>
+    <p style="margin-bottom:0px;">An account has been created for you on BBEvents. You can log in through the app and change your details using the following credentials:</p>
+    <p style="margin:0px;">Email: <strong>{{ $receiver }}</strong></p>
+    <p style="margin:0px;">Temporary password: <strong>{{ $password }}</strong></p>
+    <p>Make sure to change your password when you first log in!</p>
+</div style="margin-bottom:10px;">
 
-<p style="margin-bottom:0px;">An account has been created for you on BBEvents. You can log in through the app and change your details using the following credentials:</p>
-<p style="margin:0px;">Email: <strong>{{ $receiver }}</strong></p>
-<p style="margin:0px;">Temporary password: <strong>{{ $password }}</strong></p>
-<p>Make sure to change your password when you first log in!</p><br/>
+<a href="https://play.google.com/store/apps/details?id=com.bbehapp" target="_blank">Get the app on the Google Play Store and start editing your profile right now!</a>
 
-<!-- TODO: link to app in playstore -->
-
-<p>Happy conferencing!</p>
-<p>Adriaan at BBEvents</p>
+<div style="margin-top:10px;">
+    <p style="margin:0px;">Happy conferencing!</p>
+    <p style="margin:0px;">Adriaan at BBEvents</p>
+</div>
